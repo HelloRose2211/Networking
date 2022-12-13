bef5fee02d2f1b4a3a950fbb57e7d9f8307ce746
Rose Khaldeeva <khalrose@mail.ru>
2022-12-13 21:12:49 +0600

    Add UserActionCell

---
+++ Networking/UserActionCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserActionCell.swift
+//  Networking
+//
+//  Created by Rose on 13.12.2022.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    @IBOutlet var userActionLabel: UILabel!
+}
