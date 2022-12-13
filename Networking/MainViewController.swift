092996bfb8498284826f585d427c4541a0b4eb70
Rose Khaldeeva <khalrose@mail.ru>
2022-12-13 21:12:33 +0600

    Add MainViewController

---
+++ Networking/MainViewController.swift
@@ -0,0 +1,105 @@
+//
+//  MainViewController.swift
+//  Networking
+//
+//  Created by Rose on 13.12.2022.
+//
+
+import UIKit
+
+enum Link: String {
+    case imageURL = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+    case exampleOne = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+    case exampleTwo = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
+    case exampleThree = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+    case exampleFour = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
+}
+
+enum UserAction: String, CaseIterable {
+    case downloadImage = "Download Image"
+    case exampleOne = "Example One"
+    case exampleTwo = "Example Two"
+    case exampleThree = "Example Three"
+    case exampleFour = "Example Four"
+    case ourCourses = "Our Courses"
+}
+
+
+
+class MainViewController: UICollectionViewController {
+
+    let userActions = UserAction.allCases
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell
+        
+        
+        
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+// MARK: - Networking
+extension MainViewController {
+    private func exampleOneButtonPressed() {
+        
+            
+    
+    }
+    
+    private func exampleTwoButtonPressed() {
+        
+    }
+    
+    private func exampleThreeButtonPressed() {
+        
+    }
+    
+    private func exampleFourButtonPressed() {
+        
+    }
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width, height: 100)
+    }
+}
