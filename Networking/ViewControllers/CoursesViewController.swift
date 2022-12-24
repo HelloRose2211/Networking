4f889a502a2380a4536fc2be5323b3efc79eafe0
Rose Khaldeeva <khalrose@mail.ru>
2022-12-24 19:14:36 +0600

    Set up MainViewController

--- Networking/ViewControllers/CoursesViewController.swift
+++ Networking/ViewControllers/CoursesViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import Alamofire
 
 class CoursesViewController: UITableViewController {
     
-    private var courses: [Course] = []
+    var courses: [Course] = []
+    var coursesV2: [CourseV2] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +21,20 @@ class CoursesViewController: UITableViewController {
     // MARK: - Table view data source
 
    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       courses.count
+       courses.isEmpty ? coursesV2.count : courses.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CourseCell
-        let course = courses[indexPath.row]
-        cell.configure(with: course)
+        
+        if courses.isEmpty {
+            let courseV2 = coursesV2[indexPath.row]
+            cell.configure(with: courseV2)
+        } else {
+            let course = courses[indexPath.row]
+            cell.configure(with: course)
+        }
+
         return cell
     }
 }
@@ -33,23 +42,60 @@ class CoursesViewController: UITableViewController {
 //MARK: Networking
 extension CoursesViewController {
     func fetchCourses() {
-        guard let url  = URL(string: Link.exampleTwo.rawValue) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-        guard let data = data else {
-            print(error?.localizedDescription ?? "No error description")
-            return
-        }
-            
-            do {
-                self.courses = try JSONDecoder().decode([Course].self, from: data)
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            } catch let error {
-            print(error.localizedDescription)
+        NetworkManager.shared.fetch(dataType: [Course].self,
+                                    from: Link.exampleTwo.rawValue) { result in
+            switch result {
+            case .success(let courses):
+                self.courses = courses
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
             }
-            
-        }.resume()
+    }
+}
+
+    func fetchCoursesV2() {
+        NetworkManager.shared.fetch(dataType: [CourseV2].self,
+                                    from: Link.exampleFive.rawValue,
+                                    convertFromSnakeCase: false) { result in
+            switch result {
+            case .success(let courses):
+                self.coursesV2 = courses
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func alamofireGetButtonPressed() {
+        NetworkManager.shared.fetchDataWithAlamofire(Link.exampleTwo.rawValue) { result in
+            switch result {
+            case .success(let courses):
+                self.courses = courses
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
+    
+    func alamofirePostButtonPressed() {
+        let course = CourseV3(
+            name: "Networking",
+            imageUrl: Link.courseImageURL.rawValue,
+            numberOfLessons: "10",
+            numberOfTests: "5"
+        )
+        NetworkManager.shared.postDataWithAlamofire(Link.postRequest.rawValue, data: course) { result in
+            switch result {
+            case .success(let courses):
+                self.courses.append(course)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
