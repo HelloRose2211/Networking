62ca2c7ca77111b887f16ddbfc70e619aa04dade
Rose Khaldeeva <khalrose@mail.ru>
2022-12-24 19:00:31 +0600

    Set up CourseCell

--- Networking/CourseCell.swift
+++ Networking/CourseCell.swift
@@ -8,20 +8,37 @@
 import UIKit
 
 class CourseCell: UITableViewCell {
-    @IBOutlet var courseImage: UIImageView!
-    @IBOutlet var courseNameLabel: UILabel!
-    @IBOutlet var numberOfLessond: UILabel!
-    @IBOutlet var numberOfTests: UILabel!
+    
+    
+    @IBOutlet weak var courseImage: UIImageView!
+    @IBOutlet weak var courseNameLabel: UILabel!
+    @IBOutlet weak var numberOfLessons: UILabel!
+    @IBOutlet weak var numberOfTests: UILabel!
     
     func configure(with course: Course) {
         courseNameLabel.text = course.name
-        numberOfLessond.text = "Number of lessons: \(course.number_of_lessons ?? 0)"
-        numberOfTests.text = "Number of tests: \(course.number_of_tests ?? 0)"
-        DispatchQueue.global().async {
-            guard let url = URL(string: course.imageUrl ?? "") else { return }
-            guard let imageData = try? Data(contentsOf: url) else { return }
-            DispatchQueue.main.async {
+        numberOfLessons.text = "Number of lessons: \(course.numberOfLessons ?? 0)"
+        numberOfTests.text = "Number of tests: \(course.numberOfTests ?? 0)"
+        NetworkManager.shared.fetchImage(from: course.imageUrl) { result in
+            switch result {
+            case .success(let imageData):
                 self.courseImage.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+        }
+    }
+}
+    
+    func configure(with courseV2: CourseV2) {
+        courseNameLabel.text = courseV2.name
+        numberOfLessons.text = "Number of lessons: \(courseV2.numberOfLessons ?? 0)"
+        numberOfTests.text = "Number of tests: \(courseV2.numberOfTests ?? 0)"
+        NetworkManager.shared.fetchImage(from: courseV2.imageUrl) { result in
+            switch result {
+            case .success(let imageData):
+                self.courseImage.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
             }
         }
     }
